d50ba12eac521a44385b540a4cc5472f4a6de505
Favour Shaw <favourvanderwaal@gmail.com>
2025-05-06 09:41:05 +0100

    Xircar push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Hero from "./home/hero";
 import About from "./home/about";
 import Stack from "./home/stack";
+import Testimonial from "./home/testimonial";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <About />
       <Stack />
+      <Testimonial />
     </>
   );
 }
